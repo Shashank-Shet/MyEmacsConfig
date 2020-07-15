b65f2aaadd8574769a35b40fcd43e9dc5856b02f
Shashank Shet <shashankshet@gmail.com>
2020-07-15 11:07:37 +0530

    Removed org-mode banded appearance

--- zenburn-custom-theme.el
+++ zenburn-custom-theme.el
@@ -1,9 +1,8 @@
 (deftheme zenburn-custom
-  "Created 2020-06-07.")
+  "Created 2020-07-15.")
 
 (custom-theme-set-faces
  'zenburn-custom
- '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :width normal :height 128 :weight normal :slant normal :underline nil :overline nil :strike-through nil :box nil :inverse-video nil :foreground "AntiqueWhite1" :background "#3F3F3F" :stipple nil :inherit nil))))
  '(cursor ((t (:foreground "#DCDCCC" :background "#FFFFEF"))))
  '(fixed-pitch ((t (:family "Monospace"))))
  '(variable-pitch ((((type w32)) (:foundry "outline" :family "Arial")) (t (:family "Sans Serif"))))
@@ -46,6 +45,8 @@
  '(lazy-highlight ((t (:weight bold :foreground "#D0BF8F" :background "#383838"))))
  '(match ((t (:weight bold :foreground "#DFAF8F" :background "#2B2B2B"))))
  '(next-error ((t (:inherit (region)))))
- '(query-replace ((t (:inherit (isearch))))))
+ '(query-replace ((t (:inherit (isearch)))))
+ '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :width normal :height 128 :weight normal :slant normal :underline nil :overline nil :strike-through nil :box nil :inverse-video nil :foreground "AntiqueWhite1" :background "#3F3F3F" :stipple nil :inherit nil))))
+ '(org-block ((t (:background "#3f3f3f" :inherit fixed-pitch)))))
 
 (provide-theme 'zenburn-custom)
